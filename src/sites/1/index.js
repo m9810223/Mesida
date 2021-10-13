2aa43aaf47920463281501dc6e5826b1e53e3ff7
m9810223 <m9810223@gmail.com>
2021-10-13 15:20:09 +0800

    upd

--- src/sites/1/index.js
+++ src/sites/1/index.js
@@ -1,14 +1,8 @@
-// import { logger } from 'core';
-
 import Tracker from 'core/Tracker';
 
-// import DefaultPublisher from 'core/Tracker/Publishers/DefaultPublisher';
 import FacebookBasecode from 'core/Tracker/Publishers/FacebookBasecode';
 import GoogleAnalytics from 'core/Tracker/Publishers/GoogleAnalytics';
-// import GoogleAds from 'core/Tracker/Publishers/GoogleAds';
 
-// import DOMLoad from 'core/Site/Triggers/DOMLoad';
-// import HashChange from 'core/Site/Triggers/HashChange';
 import HistoryPush from 'core/Site/Triggers/HistoryPush';
 import PageView from 'core/Site/Triggers/PageView';
 import WindowLoad from 'core/Site/Triggers/WindowLoad';
@@ -16,10 +10,6 @@ import WindowLoad from 'core/Site/Triggers/WindowLoad';
 const trackerA = new Tracker({
   currency: 'USD',
   publishers: [
-    // new DefaultPublisher({
-    //   ids: ['11', '22'],
-    //   a: 3,
-    // }),
     new GoogleAnalytics({
       ids: ['ga1', 'ga2'],
     }),
@@ -33,9 +23,6 @@ const trackerA = new Tracker({
       ids: ['349034896886221'],
     }),
   ],
-  // conversions: [],
-  // customEvents: [],
-  // blacklist: ['ViewContent'],
 });
 
 export default {
